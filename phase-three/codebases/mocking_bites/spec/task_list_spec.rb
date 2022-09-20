842345016de75c011d09c532018e7339eb4a98e9
kateusacova <kateusacova@gmail.com>
2022-09-20 11:15:52 +0100

    Unit testing for TaskList using mocking

--- phase-three/codebases/mocking_bites/spec/task_list_spec.rb
+++ phase-three/codebases/mocking_bites/spec/task_list_spec.rb
@@ -11,5 +11,34 @@ RSpec.describe TaskList do
     expect(task_list.all_complete?).to eq false
   end
 
-  # Unit test `#all` and `#all_complete?` behaviour
+  it "returns a list of tasks" do
+    list = TaskList.new
+    task_1 = double(:task)
+    task_2 = double(:task)
+    list.add(task_1)
+    list.add(task_2)
+    expect(list.all).to eq [task_1, task_2]
+  end
+
+  it "returns false if at least one of the tasks is not completed" do
+    list = TaskList.new
+    task_1 = double(:task, complete?: true)
+    task_2 = double(:task, complete?: true)
+    task_3 = double(:task, complete?: false)
+    list.add(task_1)
+    list.add(task_2)
+    list.add(task_3)
+    expect(list.all_complete?).to eq false
+  end
+
+  it "returns true if all tasks completed" do
+    list = TaskList.new
+    task_1 = double(:task, complete?: true)
+    task_2 = double(:task, complete?: true)
+    task_3 = double(:task, complete?: true)
+    list.add(task_1)
+    list.add(task_2)
+    list.add(task_3)
+    expect(list.all_complete?).to eq true
+  end
 end
